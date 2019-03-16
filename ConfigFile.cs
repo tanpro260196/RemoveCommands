bb942e60181faa962dfb00c65868e0a17cad02f7
tanpro260196 <tncao@sfu.ca>
2019-03-15 21:07:09 -0700

    Add project files.

---
+++ ConfigFile.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace RemoveCommands
+{
+    public class ConfigFile
+    {
+        #region ConfigVars
+
+        public List<string> Commands = new List<string> { "aliases", "usergroups" };
+
+        #endregion ConfigVars
+
+        public static ConfigFile Read(string path)
+        {
+            if (!File.Exists(path))
+            {
+                ConfigFile config = new ConfigFile();
+
+                File.WriteAllText(path, JsonConvert.SerializeObject(config, Formatting.Indented));
+                return config;
+            }
+            return JsonConvert.DeserializeObject<ConfigFile>(File.ReadAllText(path));
+        }
+    }
+}
---
+++ RemoveCommands.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TShockAPI;
+using Terraria;
+using TerrariaApi.Server;
+using System.Reflection;
+using System.IO;
+
+namespace RemoveCommands
+{
+    [ApiVersion(2, 1)]
+    public class RemoveCommands : TerrariaPlugin
+    {
+        #region Plugin Info
+        public override string Name => "RemoveCommands";
+        public override string Author => "BMS";
+        public override string Description => "Remove unnecessary  commands";
+        public override Version Version => Assembly.GetExecutingAssembly().GetName().Version;
+        #endregion
+
+        public ConfigFile Config = new ConfigFile();
+
+        public RemoveCommands(Main game) : base(game)
+        {
+        }
+
+        private void LoadConfig()
+        {
+            string path = Path.Combine(TShock.SavePath, "RemoveCommands.json");
+            Config = ConfigFile.Read(path);
+        }
+
+        public override void Initialize()
+        {
+            LoadConfig();
+            ServerApi.Hooks.GameInitialize.Register(this, OnInitialize);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                ServerApi.Hooks.GameInitialize.Deregister(this, OnInitialize);
+            }
+            base.Dispose(disposing);
+        }
+        private void OnInitialize(EventArgs args)
+        {
+            foreach (string temp in Config.Commands)
+            {
+                for (int i = 0; i < Commands.ChatCommands.Count; i++)
+                {
+                    if (Commands.ChatCommands[i].Names.Contains(temp))
+                    {
+                        Commands.ChatCommands.Remove(Commands.ChatCommands[i]);
+                        continue;
+                    }
+                }
+            }
+        }
+    }
+}
